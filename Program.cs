7d2ccb5e6cb465903180d828453f3b227b97523d
xxsweet <xxsweet07@gmail.com>
2023-05-12 21:28:41 -0300

    Rolagem de dados

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿﻿using System.Security.Cryptography;
+
+Console.ForegroundColor = ConsoleColor.Red;
+
+Console.WriteLine("--- Rolagem de Dado ---\n");
+
+Console.ResetColor();
+
+Console.Write("Quantas faces? ");
+int faces = Convert.ToInt32(Console.ReadLine());
+
+int rolagem = RandomNumberGenerator.GetInt32(1, faces + 1);
+
+Console.WriteLine($"\nRolagem: {rolagem}");
